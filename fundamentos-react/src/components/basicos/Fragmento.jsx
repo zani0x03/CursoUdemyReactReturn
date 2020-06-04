4da79d76c8c34ec288fc4ffdd5446972cff6c908
zani0x03 <zani0x03@gmail.com>
2020-06-04 20:37:30 -0300

    a

---
+++ fundamentos-react/src/components/basicos/Fragmento.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Fragmento(props){
+    return (
+        <React.Fragment>
+            <h2>Fragmento</h2>
+            <h3>Cuidado com esse erro!!!</h3>
+        </React.Fragment>
+    )
+}
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import Primeiro from './components/basicos/Primeiro'
-
+import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 //const tag = <strong>Olá React</strong>
 
 ReactDOM.render(<div>
     <Primeiro></Primeiro>
-    <Primeiro></Primeiro>
+    <ComParametro titulo="Segundo componente" subtitulo="Muito legal!!!"></ComParametro>
+    <Fragmento></Fragmento>
 </div>, 
 document.getElementById('root')
 )
